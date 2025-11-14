e720437ec40fca60d9907e693cabeb243dcfb17e
katemaximova <ekaterinamaximovaqa@gmail.com>
2025-11-14 14:41:30 +0300

    - добавляем APIClient для тестов API (Booking) - Определение тестового или продакшен окружения - Автоматический выбор URL для запросов - Базовая настройка HTTP-сессии - Проверка корректности настроек

---
+++ core/clients/endpoints.py
@@ -0,0 +1,12 @@
+
+class BookingEndpoints:
+    # Healthcheck
+    PING = "/ping"
+
+    # Authentication
+    AUTH = "/auth"
+
+    # Bookings
+    BOOKING = "/booking"
+    BOOKING_BY_ID = "/booking/{booking_id}"
+
